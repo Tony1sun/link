8aa56eb425eaef43643a9e154c2e70ed7db6511b
Tony1sun <jacktony822@gmail.com>
2021-08-19 14:24:34 +0800

    提交

--- examples/ex1/main.go
+++ examples/ex1/main.go
@@ -11,7 +11,10 @@ var exampleHtml = `
 <html>
 <body>
   <h1>Hello!</h1>
-  <a href="/other-page">A link to another page</a>
+  <a href="/other-page">
+  A link to another page
+  <span> some span </span>
+  </a>
   <a href="/page-two">A link to another page</a>
 </body>
 </html>
